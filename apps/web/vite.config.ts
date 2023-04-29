44341b20d4904aa97cab7736f81f7b5b9b774377
Antonio Moruno <amoruno21@gmail.com>
2023-04-29 21:11:53 +0200

    style(web): change file structure

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,43 +1,12 @@
-/// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/web',
-
-  plugins: [
-    react(),
-    viteTsConfigPaths({
-      root: '../../',
-    }),
-  ],
-
-  preview: {
-    host: 'localhost',
-    port: 4300,
-  },
-
+  plugins: [react(), tsconfigPaths()],
   server: {
     host: 'localhost',
     port: 4200,
-  },
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: '../../',
-  //    }),
-  //  ],
-  // },
-
-  test: {
-    cache: {
-      dir: '../../node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    globals: true,
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 })
