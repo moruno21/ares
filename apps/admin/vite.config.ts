3df9f19bcf3746b3f3cf6f5a83ef338f4c06a198
Antonio Moruno <amoruno21@gmail.com>
2023-04-29 21:17:09 +0200

    style(admin): change file structure

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,23 +1,10 @@
-/// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/admin',
-
-  plugins: [
-    react(),
-    viteTsConfigPaths({
-      root: '../../',
-    }),
-  ],
-
-  preview: {
-    host: 'localhost',
-    port: 4300,
-  },
-
+  plugins: [react(), tsconfigPaths()],
   server: {
     host: 'localhost',
     port: 4200,
