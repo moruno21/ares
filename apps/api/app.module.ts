4c352ff606eebe9e2ba1a9162a56f0d8166de96e
Antonio Moruno <amoruno21@gmail.com>
2023-05-04 23:28:01 +0200

    chore(api): connect to mongo and evenstore quiero llorar

--- apps/api/app.module.ts
+++ apps/api/app.module.ts
@@ -1,11 +1,32 @@
+import {
+  EVENTSTORE_KEYSTORE_CONNECTION,
+  EventStoreModule,
+} from '@aulasoftwarelibre/nestjs-eventstore'
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { CqrsModule } from '@nestjs/cqrs'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConsoleModule } from 'nestjs-console'
 
+import configuration from './config/configuration'
 import { LoggerMiddleware } from './middleware/logger'
 
 @Module({
-  controllers: [],
-  imports: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    CqrsModule,
+    ConsoleModule,
+    EventStoreModule.forRoot({
+      connection: process.env.EVENTSTORE_URL || '',
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL || '', {}),
+    MongooseModule.forRoot(process.env.KEYSTORE_URI || '', {
+      connectionName: EVENTSTORE_KEYSTORE_CONNECTION,
+    }),
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
