f786ca9bbc0c16e2b7983627c53e20c8b0253d30
Antonio Moruno <amoruno21@gmail.com>
2023-07-15 23:40:12 +0200

    test(api): add scenario to exercise deleted hanlder test

--- apps/api/exercise/application/event-handlers/exercise-deleted.spec.ts
+++ apps/api/exercise/application/event-handlers/exercise-deleted.spec.ts
@@ -1,5 +1,6 @@
 import ExerciseDeleted from '~/exercise/domain/events/exercise-deleted'
-import Either from '~/shared/either'
+import NotFoundExercise from '~/exercise/domain/exceptions/not-found'
+import Either, { Left } from '~/shared/either'
 import ExerciseViewsMock from '~/test/mocks/exercise/application/services/views'
 
 import ExerciseView from '../models/view'
@@ -28,5 +29,24 @@ describe('ExerciseDeletedHandler', () => {
 
     await deletedHandler.handle(ExerciseDeleted.with({ id }))
     expect(viewsDelete).toHaveBeenCalledWith(view)
+  })
+
+  it('cannot delete an exercise view that does not exist', async () => {
+    const id = 'id'
+    const notFound = NotFoundExercise.withId(id)
+
+    const viewsDelete = jest.spyOn(views, 'delete')
+    const viewsWithId = jest.spyOn(views, 'withId')
+
+    viewsWithId.mockResolvedValue(Either.left(notFound))
+
+    const response = (await deletedHandler.handle(
+      ExerciseDeleted.with({ id }),
+    )) as Left<NotFoundExercise>
+
+    expect(viewsDelete).not.toHaveBeenCalled()
+    expect(Either.isRight(response)).toBe(false)
+    expect(response.value.__name__).toBe(notFound.__name__)
+    expect(response.value.code).toBe(notFound.code)
   })
 })
