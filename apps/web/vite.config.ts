a6e892ee4e734cf7027bfc99c645b93b447cc94f
Antonio Moruno <amoruno21@gmail.com>
2023-07-28 14:50:52 +0200

    chore(web): add vite svgr plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/web',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), svgr(), tsconfigPaths()],
   server: {
     host: 'localhost',
     port: 4200,
--- libs/ui/vite.config.ts
+++ libs/ui/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { join } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import svgr from 'vite-plugin-svgr'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
@@ -42,6 +43,7 @@ export default defineConfig({
       tsConfigFilePath: join(__dirname, 'tsconfig.lib.json'),
     }),
     react(),
+    svgr(),
     viteTsConfigPaths({
       root: '../../',
     }),
