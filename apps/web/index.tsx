fdc896cc26b78cc7380331c4b9ad256f000c6b4d
Antonio Moruno <amoruno21@gmail.com>
2023-08-07 13:50:03 +0200

    chore(web): config graphql

--- apps/web/index.tsx
+++ apps/web/index.tsx
@@ -1,5 +1,6 @@
 import './config/i18n'
 
+import { ApolloProvider } from '@apollo/client'
 import GlobalStyles from '@ares/ui/styles/global'
 import theme from '@ares/ui/styles/theme'
 import { StrictMode } from 'react'
@@ -7,13 +8,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
-import AppRoutes from './services/routing/Routes'
+import client from '~/graphql/client'
+import AppRoutes from '~/services/routing/Routes'
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyles />
     <BrowserRouter>
-      <AppRoutes />
+      <ApolloProvider client={client}>
+        <AppRoutes />
+      </ApolloProvider>
     </BrowserRouter>
   </ThemeProvider>
 )
