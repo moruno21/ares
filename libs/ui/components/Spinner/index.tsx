2a428610241506986e610ac26a531c76d8c49fef
Antonio Moruno <amoruno21@gmail.com>
2023-08-07 17:50:08 +0200

    feat(ui): export default memo spinner

--- libs/ui/components/Spinner/index.tsx
+++ libs/ui/components/Spinner/index.tsx
@@ -1,5 +1,7 @@
+import { memo } from 'react'
+
 import { Component } from './styles'
 
 const Spinner = ({ ...props }) => <Component aria-hidden {...props} />
 
-export default Spinner
+export default memo(Spinner)
