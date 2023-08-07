3a77a1b0772348a0987d2060f7de9ce11aebd3d5
Antonio Moruno <amoruno21@gmail.com>
2023-08-07 15:56:58 +0200

    fix(web): add suspense to routes

--- apps/web/services/routing/Routes/index.tsx
+++ apps/web/services/routing/Routes/index.tsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react'
+import { lazy, Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import Exercises from '~/pages/exercises'
@@ -9,14 +9,16 @@ const Layout = lazy(() => import('~/pages/layout'))
 const Routines = lazy(() => import('~/pages/routines'))
 
 const AppRoutes = () => (
-  <Routes>
-    <Route element={<Layout />}>
-      <Route path={ROUTES.ROUTINES} element={<Routines />}></Route>
-      <Route path={ROUTES.EXPLORE} element={<>Explore</>}></Route>
-      <Route path={ROUTES.EXERCISES} element={<Exercises />}></Route>
-      <Route path="*" element={<Navigate to={ROUTES.ROUTINES} />}></Route>
-    </Route>
-  </Routes>
+  <Suspense>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path={ROUTES.ROUTINES} element={<Routines />}></Route>
+        <Route path={ROUTES.EXPLORE} element={<>Explore</>}></Route>
+        <Route path={ROUTES.EXERCISES} element={<Exercises />}></Route>
+        <Route path="*" element={<Navigate to={ROUTES.ROUTINES} />}></Route>
+      </Route>
+    </Routes>
+  </Suspense>
 )
 
 export default AppRoutes
