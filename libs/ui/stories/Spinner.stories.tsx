3fad893d5fe3d9af671d248175e76140e61dca12
Antonio Moruno <amoruno21@gmail.com>
2023-08-07 16:52:06 +0200

    feat(ui): spinner component story

---
+++ libs/ui/stories/Spinner.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryFn } from '@storybook/react'
+
+import Component from '../components/Spinner'
+
+const meta: Meta<typeof Component> = {
+  component: Component,
+  title: 'Atoms/Spinner',
+}
+
+export default meta
+
+const Template: StoryFn<typeof Component> = (args) => <Component {...args} />
+
+export const Spinner = Template.bind({})
