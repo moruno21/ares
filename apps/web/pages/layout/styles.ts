c97fa5248f388f2658d682b51be8baa5a20ce18a
Antonio Moruno <amoruno21@gmail.com>
2023-08-11 20:57:57 +0200

    style(web): reduce padding on smaller screens

--- apps/web/pages/layout/styles.ts
+++ apps/web/pages/layout/styles.ts
@@ -1,6 +1,16 @@
 import styled from 'styled-components'
 
+import pxToRem from '~/lib/px-to-rem'
+
 export const Container = styled.div`
   width: 100%;
   padding: 3rem 4rem 5rem;
+
+  @media (max-width: ${({ theme }) => pxToRem(theme.breakpoints.medium)}) {
+    padding: 3rem 2rem 5rem;
+  }
+
+  @media (max-width: ${({ theme }) => pxToRem(theme.breakpoints.small)}) {
+    padding: 2rem 1rem 4rem;
+  }
 `
