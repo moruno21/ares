9ae658ac37b5203413e6a40cfde850aff2f59d04
Antonio Moruno <amoruno21@gmail.com>
2023-08-13 18:02:35 +0200

    feat(web): add labels to exercise form

--- apps/web/pages/exercises/Form/index.tsx
+++ apps/web/pages/exercises/Form/index.tsx
@@ -1,9 +1,16 @@
 import Button from '@ares/ui/components/Button'
 import Input from '@ares/ui/components/Input'
-import { ErrorMessage, Field } from 'formik'
+import { ErrorMessage, Field as FormikField } from 'formik'
 
 import useForm from './hooks'
-import { Container, Content, CustomErrorMessage, Fields } from './styles'
+import {
+  Container,
+  Content,
+  CustomErrorMessage,
+  Field,
+  Fields,
+  Label,
+} from './styles'
 import { FormProps } from './types'
 
 const Form = ({ error }: FormProps) => {
@@ -13,24 +20,34 @@ const Form = ({ error }: FormProps) => {
     <Container>
       <Content>
         <Fields>
-          <Field
-            as={Input}
-            name="name"
-            onChange={handleChangeName}
-            placeholder={t('form.inputs.name.placeholder')}
-          />
-          <ErrorMessage name="name">
-            {(msg) => <CustomErrorMessage>{msg}</CustomErrorMessage>}
-          </ErrorMessage>
-          <Field
-            as={Input}
-            name="description"
-            onChange={handleChangeDescription}
-            placeholder={t('form.inputs.description.placeholder')}
-          />
-          <ErrorMessage name="description">
-            {(msg) => <CustomErrorMessage>{msg}</CustomErrorMessage>}
-          </ErrorMessage>
+          <Field>
+            <Label htmlFor="exercise_name">{t('form.inputs.name.label')}</Label>
+            <FormikField
+              as={Input}
+              id="exercise_name"
+              name="name"
+              onChange={handleChangeName}
+              placeholder={t('form.inputs.name.placeholder')}
+            />
+            <ErrorMessage name="name">
+              {(msg) => <CustomErrorMessage>{msg}</CustomErrorMessage>}
+            </ErrorMessage>
+          </Field>
+          <Field>
+            <Label htmlFor="exercise_description">
+              {t('form.inputs.description.label')}
+            </Label>
+            <FormikField
+              as={Input}
+              id="exercise_description"
+              name="description"
+              onChange={handleChangeDescription}
+              placeholder={t('form.inputs.description.placeholder')}
+            />
+            <ErrorMessage name="description">
+              {(msg) => <CustomErrorMessage>{msg}</CustomErrorMessage>}
+            </ErrorMessage>
+          </Field>
         </Fields>
         <Button type="submit">{t('form.save.label')}</Button>
       </Content>
