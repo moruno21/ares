1d7fd70310deda0e315564b6a1681ec5d7576b8f
Antonio Moruno <amoruno21@gmail.com>
2023-08-13 17:53:51 +0200

    feat(web): add labels to routine creation form

--- apps/web/pages/routines/Form/index.tsx
+++ apps/web/pages/routines/Form/index.tsx
@@ -1,9 +1,16 @@
 import Button from '@ares/ui/components/Button'
 import Input from '@ares/ui/components/Input'
-import { ErrorMessage, Field } from 'formik'
+import { ErrorMessage, Field as FormikField } from 'formik'
 
 import useForm from './hooks'
-import { Container, Content, CustomErrorMessage, Fields } from './styles'
+import {
+  Container,
+  Content,
+  CustomErrorMessage,
+  Field,
+  Fields,
+  Label,
+} from './styles'
 
 const Form = () => {
   const { handleChangeDescription, handleChangeName, t } = useForm()
@@ -12,24 +19,34 @@ const Form = () => {
     <Container>
       <Content>
         <Fields>
-          <Field
-            as={Input}
-            name="name"
-            onChange={handleChangeName}
-            placeholder={t('form.inputs.name.placeholder')}
-          />
-          <ErrorMessage name="name">
-            {(msg) => <CustomErrorMessage>{msg}</CustomErrorMessage>}
-          </ErrorMessage>
-          <Field
-            as={Input}
-            name="description"
-            onChange={handleChangeDescription}
-            placeholder={t('form.inputs.description.placeholder')}
-          />
-          <ErrorMessage name="description">
-            {(msg) => <CustomErrorMessage>{msg}</CustomErrorMessage>}
-          </ErrorMessage>
+          <Field>
+            <Label htmlFor="routine_name">{t('form.inputs.name.label')}</Label>
+            <FormikField
+              as={Input}
+              id="routine_name"
+              name="name"
+              onChange={handleChangeName}
+              placeholder={t('form.inputs.name.placeholder')}
+            />
+            <ErrorMessage name="name">
+              {(msg) => <CustomErrorMessage>{msg}</CustomErrorMessage>}
+            </ErrorMessage>
+          </Field>
+          <Field>
+            <Label htmlFor="routine_description">
+              {t('form.inputs.description.label')}
+            </Label>
+            <FormikField
+              as={Input}
+              id="routine_description"
+              name="description"
+              onChange={handleChangeDescription}
+              placeholder={t('form.inputs.description.placeholder')}
+            />
+            <ErrorMessage name="description">
+              {(msg) => <CustomErrorMessage>{msg}</CustomErrorMessage>}
+            </ErrorMessage>
+          </Field>
         </Fields>
         <Button type="submit">{t('form.save.label')}</Button>
       </Content>
