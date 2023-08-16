7e5d494b75dbe528afa0b4358d49ce499c8e79eb
Antonio Moruno <amoruno21@gmail.com>
2023-08-17 00:31:49 +0200

    style(web): routes

--- apps/web/services/routing/Routes/index.tsx
+++ apps/web/services/routing/Routes/index.tsx
@@ -5,42 +5,33 @@ import PageLoading from '~/pages/layout/Loading'
 
 import { ROUTES } from './constants'
 
-const ExercisesLayout = lazy(() => import('~/pages/exercises'))
+const Exercises = lazy(() => import('~/pages/exercises'))
 const Explore = lazy(() => import('~/pages/explore'))
-const LandingLayout = lazy(() => import('~/pages/landing'))
-const AuthLayout = lazy(() => import('~/pages/auth'))
+const Landing = lazy(() => import('~/pages/landing'))
+const Auth = lazy(() => import('~/pages/auth'))
 const Layout = lazy(() => import('~/pages/layout'))
 const PreventAuth = lazy(() => import('../PreventAuth'))
 const RequireAuth = lazy(() => import('../RequireAuth'))
-const RoutineLayout = lazy(() => import('~/pages/routine'))
-const RoutinesLayout = lazy(() => import('~/pages/routines'))
+const Routine = lazy(() => import('~/pages/routine'))
+const Routines = lazy(() => import('~/pages/routines'))
 
 const AppRoutes = () => (
   <Suspense fallback={<PageLoading />}>
     <Routes>
       <Route element={<PreventAuth />}>
-        <Route path={`${ROUTES.LOGIN}`} element={<AuthLayout />}></Route>
+        <Route path={`${ROUTES.LOGIN}`} element={<Auth />}></Route>
       </Route>
       <Route element={<RequireAuth />}>
         <Route element={<Layout />}>
-          <Route path={ROUTES.HOME} element={<RoutinesLayout />}></Route>
-          <Route
-            path={`${ROUTES.USER}/:id`}
-            element={<RoutinesLayout />}
-          ></Route>
-          <Route
-            path={`${ROUTES.ROUTINE}/:id`}
-            element={<RoutineLayout />}
-          ></Route>
+          <Route path={ROUTES.HOME} element={<Routines />}></Route>
+          <Route path={`${ROUTES.USER}/:id`} element={<Routines />}></Route>
+          <Route path={`${ROUTES.ROUTINE}/:id`} element={<Routine />}></Route>
           <Route path={ROUTES.EXPLORE} element={<Explore />}></Route>
-          <Route path={ROUTES.EXERCISES} element={<ExercisesLayout />}></Route>
+          <Route path={ROUTES.EXERCISES} element={<Exercises />}></Route>
           <Route path="*" element={<Navigate to={ROUTES.HOME} />}></Route>
         </Route>
       </Route>
-      <Route
-        path={`${ROUTES.LANDING}/:idHash`}
-        element={<LandingLayout />}
-      ></Route>
+      <Route path={`${ROUTES.LANDING}/:idHash`} element={<Landing />}></Route>
     </Routes>
   </Suspense>
 )
